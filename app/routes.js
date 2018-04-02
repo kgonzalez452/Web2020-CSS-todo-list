b18265cf4f7b6967d8ef95a3c05692f40bb38105
kgonzalez452 <kgonzalez@techlaunch.io>
2018-04-02 17:14:32 -0400

    delete and complete working

--- app/routes.js
+++ app/routes.js
@@ -3,7 +3,7 @@ var moment = require('moment');
 
 module.exports = function (app, database) { //exports a whole funcyion of the following routes to be able to use this function in other files using a require('); method.
 
-  app.get('/', function (req, res) { // this does nothing but send a request to an address until you code whats inside this fucntion 
+  app.get('/', function (req, res) { // this does nothing but send a request to an address until your code whats inside this fucntion 
     res.render('index.ejs', {     // which sends a response to the server that serves the index page
       user: {                         //and this sends an object within the index page to get the user
         name: 'Kevin Gonzalez',
@@ -12,10 +12,10 @@ module.exports = function (app, database) { //exports a whole funcyion of the fo
   });
   //create a request to this path
   app.get('/get-todos', function (req, res) { //sends a request to an address of localhost:6969/get-todos.
-
     database.query(                           //this creates a query in the database youre posting to
       `SELECT * FROM todos`,                  //this is the query youre sending
       function (error, results, fields) {     //this function is how you handle errors 
+        console.log(results);
 
         if (error) throw error;
 
@@ -41,9 +41,9 @@ module.exports = function (app, database) { //exports a whole funcyion of the fo
 
     database.query(
       `INSERT INTO todos(task, date, complete, uid, due_date) 
-       VALUES('${todo.task}', '${todo.date}', ${todo.completed}, '${todo.uid}', '${todo.due_date}')`, 
+       VALUES('${todo.task}', '${todo.date}', '${todo.complete}', '${todo.uid}', '${todo.due_date}')`, 
       function (error, result, fields) {
-
+        console.log(result);
         if (error) {
           console.log('error ', error);
 
@@ -56,40 +56,54 @@ module.exports = function (app, database) { //exports a whole funcyion of the fo
         else {
           // console.log('result: ', result);
           todo.id = result.insertId;
+
           res.send({
             success: true,
             todo: todo
           });
         }
     });
+
   });
 
   app.post('/update-todo', function (req, res) {
     var id = req.body.id;
+
     database.query(
       `UPDATE todos SET complete = true WHERE id = ${id}`,
-      function (error, results, fields) {
+      function (error, result, fields) {
+        console.log(result);
+        if (error) {
+          console.log('error ', error);
 
-        if (error) throw error;
+          res.send({
+            success: false,
+            error: error,
+            message: 'The todo was not updated :('
+          });
+        }
+        else {
+          console.log('result: ', result);
 
-        console.log('results: ', results);
+          res.send({
+            success: true,
+            id: id
+          });
+        }
 
-        res.render('todos.ejs', { // returns and loads the page 'todos.ejs'
-          todos: results
-        });
       });
   });
 
   app.post('/delete-todo', function (req, res) {
     var id = req.body.id;
-
+    console.log(id);
     database.query(
       `DELETE FROM todos WHERE id = ${id}`,
       function (error, result, fields) {
-
+        
         if (error) {
           console.log('error ', error);
-
+          
           res.send({
             success: false,
             error: error,
@@ -98,14 +112,15 @@ module.exports = function (app, database) { //exports a whole funcyion of the fo
         }
         else {
           console.log('result: ', result);
-         
+          
           res.send({
             success: true,
             id: id
           });
         }
       });
-
+      
+      console.log(id);
   });
 
 
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,51 +1,76 @@
 $(document).ready(function(){
-    const scroll = () => $('#todos_container').scrollTop($('#todos_container')[0].scrollHeight);
-  
-    const createTodo = function() {
-      let task = $('#todo_input').val();
-      if (task) {
-        $.post("/create-todo", {
-          task: task
-        }, function (response) {
-          if (response.success) {
-            console.log('result ', response.todo);
-  
-            $('#todos_container').append(`
-              <div id="${response.todo.id}" class="todo-container ${response.todo.complete ? 'checked' : ''}">
-                <div class="remove">
-                  <span class="ion-close-round icon-remove"></span>
-                </div>
-                <div data-complete="${response.todo.complete}" class="todo">
-                  ${response.todo.task}
-                </div>
+  const scroll = () => $('#todos_container').scrollTop($('#todos_container')[0].scrollHeight);
+
+  const createTodo = function() {
+    let task = $('#todo_input').val();
+    if (task) {
+      $.post("/create-todo", {
+        task: task
+      }, function (response) {
+        if (response.success) {
+          console.log('result ', response.todo);
+
+          $('#todos_container').append(`
+            <div id="${response.todo.id}" class="todo-container ${response.todo.complete ? 'checked' : ''}">
+              <div class="remove">
+                <span class="ion-close-round icon-remove"></span>
               </div>
-            `);
-  
-            $('#todo_input').val('');
-  
-            scroll();
-          }
-          else {
-            alert(response.message);
-          }
-        });
+              <div data-complete="${response.todo.complete}" class="todo">
+                ${response.todo.task}
+              </div>
+            </div>
+          `);
+
+          $('#todo_input').val('');
+
+          scroll();
+        }
+        else {
+          alert(response.message);
+        }
+      });
+    }
+    else {
+      alert('You must insert a task');
+    }
+  }
+
+  const removeTodo = function(e) {
+    let id = $(this).parent()[0].id;
+
+    console.log('removing ', id);
+
+    $.post("/delete-todo", {
+      id: id
+    }, function (response) {
+      if (response.success) {
+        $('#' + response.id).remove();
+        console.log(id);
       }
       else {
-        alert('You must insert a task');
+        alert(response.message);
       }
-    }
-  
+    });
+  }
 
-    const removeTodo = function(e) {
-      let id = $(this).parent()[0].id;
-  
-      console.log('removing ', id);
-  
-      $.post("/delete-todo", {
+  const updateTodo = function (e) {
+
+    let id = $(this).parent()[0].id;
+    // let complete = $(this).data('complete');
+    let complete = $(".complete").click(function() {
+      $(this).css(
+        {"text-decoration": "line-through white",
+         "color":"black"});
+    });
+
+    console.log('complete ', complete);
+
+    if (complete === false) {
+      $.post("/update-todo", {
         id: id
       }, function (response) {
         if (response.success) {
-          $('#' + response.id).remove();
+          $('#' + response.id).addClass('checked');
         }
         else {
           alert(response.message);
@@ -53,17 +78,17 @@ $(document).ready(function(){
       });
     }
 
+  }
 
+  $('#todos_container').on('click', '.remove', removeTodo);
 
-    $('#todos_container').on('click', '.remove', removeTodo);
-  
-    $('#todos_container').on('click', '.todo', updateTodo);
-  
-    $('#btn').on('click', createTodo);
-  
-    $('#todo_input').on('keyup', function (e) {
-      if(e.keyCode === 13){
-        createTodo();
-      }
-    });
+  $('#todos_container').on('click', '.todo', updateTodo);
+
+  $('#btn').on('click', createTodo);
+
+  $('#todo_input').on('keyup', function (e) {
+    if(e.keyCode === 13){
+      createTodo();
+    }
   });
+});
